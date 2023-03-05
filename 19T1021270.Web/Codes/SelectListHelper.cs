bb9e45412678f268769256065ccc7db6382ccdf0
congtien2001 <tien1972001@gmail.com>
2023-03-05 21:56:30 +0700

    update

---
+++ 19T1021270.Web/Codes/SelectListHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _19T1021270.BusinessLayers;
+using _19T1021270.DomainModels;
+using System.Web.Mvc;
+
+namespace _19T1021270.Web
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class SelectListHelper
+    {
+        /// <summary>
+        /// Danh sách Quốc gia
+        /// </summary>
+        /// <returns></returns>
+        public static List<SelectListItem> Countries()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            list.Add(new SelectListItem()
+            {
+                Value = "",
+                Text = "---Chọn Quốc gia---",
+            });
+
+            foreach (var item in CommonDataService.ListOfCountries())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = item.CountryName,
+                    Text = item.CountryName,
+                });
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Danh sách Loại hàng
+        /// </summary>
+        /// <returns></returns>
+        public static List<SelectListItem> Categories()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            list.Add(new SelectListItem()
+            {
+                Value = "",
+                Text = "---Chọn Loại hàng---",
+            });
+
+            foreach (var item in CommonDataService.ListOfCategories())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = Convert.ToString(item.CategoryID),
+                    Text = item.CategoryName,
+                });
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Danh sách Nhà cung cấp
+        /// </summary>
+        /// <returns></returns>
+        public static List<SelectListItem> Suppliers()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            list.Add(new SelectListItem()
+            {
+                Value = "",
+                Text = "---Chọn Nhà cung cấp---",
+            });
+
+            foreach (var item in CommonDataService.ListOfSuppliers())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = Convert.ToString(item.SupplierID),
+                    Text = item.SupplierName,
+                });
+            }
+
+            return list;
+        }
+    }
+}
